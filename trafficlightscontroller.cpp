fc7a1ab3e09699bec87a8ef97e85206438666f71
chiyua <chiyuan996@gmail.com>
2024-01-29 11:25:35 -0500

    Build a C++ class in back end that responsible for controlling the on and off of traffic lights

---
+++ trafficlightscontroller.cpp
@@ -0,0 +1,54 @@
+﻿#include "trafficlightscontroller.h"
+
+TrafficLightsController::TrafficLightsController(QObject *parent)
+    : QObject{parent}
+{
+
+}
+
+void TrafficLightsController::setRedSwitch(const bool &redSwitch)
+{
+    if(redSwitch==mRedSwitch){
+        return;
+    }
+    mRedSwitch=redSwitch;
+    emit redSwitchIsChanged();
+}
+
+void TrafficLightsController::setYellowSwitch(const bool &yellowSwitch)
+{
+    if(yellowSwitch==mYellowSwitch){
+        return;
+    }
+    mYellowSwitch=yellowSwitch;
+    emit yellowSwitchIsChanged();
+}
+
+void TrafficLightsController::setGreenSwitch(const bool &greenSwitch)
+{
+    if(greenSwitch==mGreenSwitch){
+        return;
+    }
+    mGreenSwitch=greenSwitch;
+    emit greenSwitchIsChanged();
+}
+
+bool TrafficLightsController::redSwitch()
+{
+    return mRedSwitch;
+}
+
+bool TrafficLightsController::greenSwitch()
+{
+    return mGreenSwitch;
+}
+
+bool TrafficLightsController::yellowSwitch()
+{
+    return mYellowSwitch;
+}
+
+void TrafficLightsController::trafficLightsController()
+{
+
+}
---
+++ trafficlightscontroller.h
@@ -0,0 +1,36 @@
+﻿#ifndef TRAFFICLIGHTSCONTROLLER_H
+#define TRAFFICLIGHTSCONTROLLER_H
+
+#include <QObject>
+#include <qqml.h>
+
+class TrafficLightsController : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TrafficLightsController(QObject *parent = nullptr);
+    Q_PROPERTY(bool redSwitch READ redSwitch WRITE setRedSwitch NOTIFY redSwitchIsChanged)
+    Q_PROPERTY(bool yellowSwitch READ yellowSwitch WRITE setYellowSwitch NOTIFY yellowSwitchIsChanged)
+    Q_PROPERTY(bool greenSwitch READ greenSwitch WRITE setGreenSwitch NOTIFY greenSwitchIsChanged)
+
+    void setRedSwitch(const bool &redSwitch);
+    void setYellowSwitch(const bool &yellowSwitch);
+    void setGreenSwitch(const bool &greenSwitch);
+
+    bool redSwitch();
+    bool yellowSwitch();
+    bool greenSwitch();
+
+    void trafficLightsController();
+
+signals:
+    void redSwitchIsChanged();
+    void yellowSwitchIsChanged();
+    void greenSwitchIsChanged();
+private:
+    bool mRedSwitch;
+    bool mYellowSwitch;
+    bool mGreenSwitch;
+};
+
+#endif // TRAFFICLIGHTSCONTROLLER_H
